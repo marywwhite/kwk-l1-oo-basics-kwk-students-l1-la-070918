559331599315a397fd4d98cadd8ad57237569ee1
Mary White <maryewhite2022@gmail.com>
2018-07-12 21:31:56 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,4 +2,28 @@
 # Add you Book class here
 
 class Book
+  
+  
+end
+
+Book,new("And Then There Were None")
+
+
+
+
+class Element
+  
+  def initialize (name, atomic_num)
+    @name = name
+    @atomic_num = atomic_num
+  end
+  
+  def name
+    @name
+  end
+  
+  def atomic_num
+    @atomic_num
+  end
+
 end
