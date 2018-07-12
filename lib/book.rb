8c599ff69d46ce44af8366d8a7693dee5a34b293
Mary White <maryewhite2022@gmail.com>
2018-07-12 21:35:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,22 +10,22 @@ Book,new("And Then There Were None")
 
 
 
-
+#template
 class Element
-  
   def initialize (name, atomic_num)
     @name = name
     @atomic_num = atomic_num
   end
-  
   def name
     @name
   end
-  
   def atomic_num
     @atomic_num
   end
-
 end
 
+#add new elements
+hydrogen = Element.new("Hydrogen",1)
+
+periodic_table =[]
 hydrogen = Element.new("Hydrogen",1)
